e6853e236f4b851f7d53e4e7f744740b594aec74
t.moechel <t.moechel@FuturaSolutionsGmbH2019.onmicrosoft.com>
2020-08-26 10:49:35 +0200

    initial checkin

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Confirm from './components/Confirm'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IState {
+  confirmOpen: boolean;
+  confirmMessage: string;
+}
+
+
+// Generic Component declaration contains <Props,State>
+// Type checking will be performed based on the generic parameiter
+class App extends React.Component<{}, IState> {
+  constructor(props: {}) {
+    super(props);
+    // state is initialized in constructor
+    this.state = {
+      confirmOpen: false,
+      confirmMessage : "Please hit the confirm button"
+    };
+  }
+  
+  handleOkClicked = () => this.setState( {
+    confirmOpen: false, 
+    confirmMessage: "Cool, carry on reading!"
+  });
+
+  handleCancelClicked = () => this.setState( {
+    confirmOpen: false,
+    confirmMessage: "Take a break, I'm sure you will later"})
+
+  handleConfirmClick = () => this.setState( {
+    confirmOpen: true
+  });
+
+  public render() {
+    return (
+      <div>
+        <h2> Hello React </h2>
+        <p>{this.state.confirmMessage}</p>
+        <button onClick={this.handleConfirmClick}>Confirm</button>
+        <Confirm
+          open={this.state.confirmOpen}
+          title="React and Typescript"
+          content="You sure you want to learn React?"
+          onOkClick={this.handleOkClicked}
+          onCancelClick={this.handleCancelClicked}
+        />
+        
+      </div>
+    )
+  }
 }
 
 export default App;
