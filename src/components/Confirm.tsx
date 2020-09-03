7f69c8f1a6f7aede8a95d0701caca68f1677b473
t.moechel <t.moechel@FuturaSolutionsGmbH2019.onmicrosoft.com>
2020-09-03 11:10:44 +0200

    stateless func. component

--- src/components/Confirm.tsx
+++ src/components/Confirm.tsx
@@ -12,46 +12,46 @@ interface ConfirmationProps {
     onCancelClick: () => void;
 }
 
-class Confirm extends React.Component<ConfirmationProps> {
-    public static defaultProps = {
-        cancelCaption: "No way",
-        okCaption: "Yes please",
-        
-    };
-    private handleOkClick = () => {
-        this.props.onOkClick();
+// Confirm as a statedless functional component SFC
+const Confirm: React.SFC<ConfirmationProps> = (props) => {
+
+    const handleOkClick = () => {
+        props.onOkClick();
     }
 
-    private handleCancelClick = () => {
-        this.props.onCancelClick();
+    const handleCancelClick = () => {
+        props.onCancelClick();
     }
 
-    public render() {
-        return (
-            <div className= {this.props.open ? 
-                "confirm-wrapper confirm-visible":
-                "confirm-wrapper"}
-            >
-                <div className="confirm-container">
-                    <div className="confirm-title-container">
-                        <span>{this.props.title}</span>
-                    </div>
-                    <div className="confirm-content-container">
-                        <p>{this.props.content}</p>
-                    </div>
-                    <div className="confirm-buttons-container">
-                        <button className="confirm-cancel" onClick={this.handleCancelClick}>
-                            {this.props.cancelCaption}
-                        </button>
-                        <button className="confirm-ok" onClick={this.handleOkClick}>
-                            {this.props.okCaption}
-                        </button>
-                    </div>
+    return (
+
+        <div className={props.open ?
+            "confirm-wrapper confirm-visible" :
+            "confirm-wrapper"}
+        >
+            <div className="confirm-container">
+                <div className="confirm-title-container">
+                    <span>{props.title}</span>
+                </div>
+                <div className="confirm-content-container">
+                    <p>{props.content}</p>
+                </div>
+                <div className="confirm-buttons-container">
+                    <button className="confirm-cancel" onClick={handleCancelClick}>
+                        {props.cancelCaption}
+                    </button>
+                    <button className="confirm-ok" onClick={handleOkClick}>
+                        {props.okCaption}
+                    </button>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
+}
 
+Confirm.defaultProps = {
+    cancelCaption: "Cancel",
+    okCaption: "Okay"
 }
 
 export default Confirm;
